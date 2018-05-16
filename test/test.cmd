3ebf340dc907916da4539478d4214e4906c703fb
Joel Martin <J_Martin@lbl.gov>
2018-05-15 22:09:05 -0700

    test files
    Former-commit-id: 3a47b0282b27e389c7296a14d23906468d937c98

---
+++ test/test.cmd
@@ -0,0 +1,3 @@
+./p2v -co 200 -P test.1006_1162994.pindel.BND -r ./mini.fa -R mini.fa -d 04-25-18 -v out
+pindel2vcf -co 200 -P test.1006_1162994.pindel.BND -r ./mini.fa -R mini.fa -d 04-25-18 -v orig
+
